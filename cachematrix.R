ecc18dff449e2aad322dcb742af6e5633d170c19
mkyana3 <mkyana3@gmail.com>
2016-08-23 15:01:53 +0200

    Submitting Programming Assigment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Thi function gets a matrix as input and then creates a list of fucntion to get or  set the matrix
+## the list returned has also the function to set or to get a matrix inverse
+## Matrix inverse and matrix itself are saved in global environement to be access in order to save time
 
-## Write a short comment describing this function
+## lexical scope use of <<- operate to define global environment variables
+##cashing both the matrix and matrix inverse
+##
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv <<- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setMatrixInv <- function(solve) inv <<- solve
+  getMatrixInv <- function () inv
+  list(set=set, get=get,setMatrixInve=setMatrixInv,getMatrixInv=getMatrixInv)
 
 }
 
 
-## Write a short comment describing this function
+## calculating the matrix inverse in case it is not in cashed
+## and caching the matrix inverse using return list from makeCacheMatrix function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <<- x$getMatrixInv()
+  if(!is.null(inv)){
+    message("printing matrix data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data,...)
+  x$setMatrixInv(inv)
+  inv
+  
 }
